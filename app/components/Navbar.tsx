83831da8fa7bd1b623c8e636a3501cd08ced3532
Bekiboo <julien.connault@gmail.com>
2024-11-01 18:23:33 +0300

    Refactor Navbar component and add it to RootLayout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+export const Navbar = () => {
+  const pathname = usePathname()
+
+  return (
+    <nav className="sticky z-50 inset-x-0 flex justify-between bg-gray-800 text-white p-4">
+      <div>
+        <a href="/" className={pathname === '/' ? 'underline' : ''}>
+          Home
+        </a>
+        <span> | </span>
+        <a
+          href="/donors"
+          className={pathname.startsWith('/donors') ? 'underline' : ''}
+        >
+          Donors
+        </a>
+        <span> | </span>
+        <a
+          href="/children"
+          className={pathname.startsWith('/children') ? 'underline' : ''}
+        >
+          Children
+        </a>
+        <span> | </span>
+        <a
+          href="/staff"
+          className={pathname.startsWith('/staff') ? 'underline' : ''}
+        >
+          Staff
+        </a>
+      </div>
+    </nav>
+  )
+}
