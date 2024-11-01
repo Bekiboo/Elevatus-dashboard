77d7e07d889d885eaba304fc9255dcff725cd6ee
Bekiboo <julien.connault@gmail.com>
2024-11-01 19:53:43 +0300

    Refactor child list and child details pages

---
+++ app/children/[childId]/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { children } from '../children'
+
+const ChildDetails: React.FC<{ params: { childId: string } }> = ({
+  params,
+}) => {
+  const router = useRouter()
+
+  const child = children.find((c) => c.id == params.childId)
+  console.log(params.childId)
+
+  if (!child) return <div>Child not found</div>
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-6">Child Details</h1>
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold mb-4">
+          {child.firstName} {child.lastName}
+        </h2>
+        <p>
+          <strong>Grade Level:</strong> {child.gradeLevel}
+        </p>
+        <p>
+          <strong>Enrollement Date:</strong> {child.enrollmentDate}
+        </p>
+        <p>
+          <strong>Adress:</strong> ${child.address}
+        </p>
+        <p>
+          <strong>Health Status:</strong> {child.healthStatus}
+        </p>
+        <p>
+          <strong>Birth Date:</strong> {child.birthdate}
+        </p>
+        <p>
+          <strong>Guardian Contact:</strong>
+          {child.guardianContact}
+        </p>
+        <p>
+          <strong>Guardian Name:</strong> {child.guardianName}
+        </p>
+        <p>
+          <strong>Last Checkup Update:</strong> {child.lastCheckupDate}
+        </p>
+        <p>
+          <strong>Comments:</strong> {child.comments}
+        </p>
+      </div>
+      <button
+        onClick={() => router.back()}
+        className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+      >
+        Back to Child List
+      </button>
+    </div>
+  )
+}
+
+export default ChildDetails
---
+++ app/children/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { children } from './children'
+import { useRouter } from 'next/navigation'
+
+const ChildrenList: React.FC = () => {
+  const router = useRouter()
+
+  const handleRowClick = (childId: string) => {
+    router.push(`/children/${childId}`)
+  }
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-6">Child List</h1>
+      <table className="min-w-full bg-white shadow-md rounded-b-lg overflow-hidden">
+        <thead className="bg-gray-800 text-white">
+          <tr>
+            <th className="px-4 py-2">First Name</th>
+            <th className="px-4 py-2">Health Status</th>
+            <th className="px-4 py-2">Enrollment Date</th>
+            <th className="px-4 py-2">Grade Level</th>
+            <th className="px-4 py-2">Last Checkup Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {children.map((child) => (
+            <tr
+              key={child.id}
+              onClick={() => handleRowClick(child.id)}
+              className="cursor-pointer hover:bg-gray-100"
+            >
+              <td className="border px-4 py-2">{child.firstName}</td>
+              <td className="border px-4 py-2">{child.healthStatus}</td>
+              <td className="border px-4 py-2">{child.enrollmentDate}</td>
+              <td className="border px-4 py-2">{child.gradeLevel}</td>
+              <td className="border px-4 py-2">{child.lastCheckupDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ChildrenList
